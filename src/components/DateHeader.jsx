ad322d19f12933aaf52ab48f74ad254be1486d49
Karthik Kumar <karthik.krishnamoorthy@nagarro.com>
2022-05-20 15:51:13 +0530

    Update date header

--- src/components/DateHeader.jsx
+++ src/components/DateHeader.jsx
@@ -1,9 +1,28 @@
-function DateHeader() {
+import PropTypes from "prop-types";
+import moment from "moment";
+function DateHeader({ date }) {
+  if (!date) {
+    return null;
+  }
+  const diff = moment().diff(date, "days", true);
+  let dateLabel;
+  if (diff <= 0) {
+    dateLabel = "Today";
+  } else if (diff > 0 && diff < 1) {
+    dateLabel = "Yesterday";
+  } else {
+    dateLabel = date.format("D, MMMM");
+  }
   return (
     <div className="date-header">
-      <p>Today, 16 February</p>
+      <span className="line"></span>
+      <p>{dateLabel}</p>
     </div>
   );
 }
+
+DateHeader.propTypes = {
+  date: PropTypes.object,
+};
 
 export default DateHeader;
