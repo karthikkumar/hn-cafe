254ced32338ff1d46bfe9dc84c06adaad1e5f2b4
Karthik Kumar <karthik.krishnamoorthy@nagarro.com>
2022-05-21 14:59:47 +0530

    News Item UI corrections

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,23 +1,52 @@
-function NewsItem({ rank, title, by, score, descendants }) {
+import y from "../img/Y.png";
+
+const openLinkProps = { target: "_blank", rel: "noreferrer noopener" };
+
+function NewsItem({ id, rank, title, url, by, score, descendants }) {
+  const { origin, hostname } = url ? new URL(url) : {};
+
+  const itemURL = `https://news.ycombinator.com/item?id=${id}`;
+  const handleItemClick = () => {
+    window.open(itemURL);
+  };
+
   return (
     <div className="item">
       <div className="rank">{rank}</div>
       <div className="news">
-        <h3>{title}</h3>
+        <a href={url || itemURL} {...openLinkProps}>
+          {title}
+        </a>
         <div className="news-info">
           <div className="news-info-left">
-            <small className="site">
-              <p>twitter.com</p>
-            </small>
+            {url && (
+              <small className="site">
+                <a href={origin} {...openLinkProps}>
+                  {hostname}
+                </a>
+              </small>
+            )}
             <small className="author">
               <p>
-                by
-                <a href={`https://news.ycombinator.com/user?id=${by}`}>{by}</a>
+                {`by `}
+                <a
+                  href={`https://news.ycombinator.com/user?id=${by}`}
+                  {...openLinkProps}
+                >
+                  {by}
+                </a>
               </p>
             </small>
           </div>
 
-          <div className="news-info-right">
+          <div
+            className="news-info-right"
+            onClick={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              handleItemClick();
+            }}
+          >
             <small className="time">
               <p>5 hours ago</p>
             </small>
@@ -27,7 +56,9 @@ function NewsItem({ rank, title, by, score, descendants }) {
             <small className="comments">
               <p>{descendants} comments</p>
             </small>
-            {/* <img src="img/Y.png" alt="" /> */}
+            <a href={itemURL} {...openLinkProps}>
+              <img src={y} alt="y combinator" />
+            </a>
           </div>
         </div>
       </div>
--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -52,7 +52,7 @@ function NewsList() {
   const rowVirtualizer = useVirtual({
     size: storiesByDates.length,
     parentRef,
-    estimateSize: useCallback(() => 400 * FilterSet[top] + 50, [top]),
+    estimateSize: useCallback(() => 380 * FilterSet[top] + 50, [top]),
   });
 
   useEffect(() => {
