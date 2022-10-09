9d21b8a0027d4f9e4bec2e7ec7de08fdb8a2ecc1
Karthik Kumar <karthik.krishnamoorthy@nagarro.com>
2022-10-09 18:44:32 +0530

    Update settings modal's z index

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -15,7 +15,7 @@ const Settings = ({ isOpen, onClose }) => {
   }
 
   return (
-    <div css={{ position: "fixed", zIndex: 1 }}>
+    <div css={{ position: "fixed", zIndex: 2 }}>
       <div
         css={{
           position: "fixed",
